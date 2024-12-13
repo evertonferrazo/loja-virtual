34d43102b1725f3f7f747b9d8ac0344290b1f5f2
Everton Ferraz <evertonferrazo@gmail.com>
2024-12-13 11:24:16 -0300

    iniciando

---
+++ backend/src/main/java/com/dev/backend/controller/Hello.java
@@ -0,0 +1,17 @@
+package com.dev.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("/api")
+public class Hello {
+    
+    @GetMapping("/")
+    public String hello(){
+        return "Olá Mundo ";
+    }
+
+}
